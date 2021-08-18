72f124d77db1dbd745ec0c6a8254b3a716497437
Lanzar-dev <78908299+Lanzar-dev@users.noreply.github.com>
2021-08-18 23:25:13 +0100

    final deployment

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,7 @@
 require('dotenv').config();
 const express = require('express');
 const connectDB = require('./config/db');
+const path = require("path");
 const productRoutes = require('./routes/productRoutes');
 
 connectDB();
@@ -9,7 +10,19 @@ const app = express();
 
 app.use(express.json());
 
-app.use('/api/products', productRoutes)
+app.use('/api/products', productRoutes);
+
+if(process.env.NODE_ENV === "production") {
+    app.use(express.static(path.join(__dirname, "../frontend/build")));
+
+    app.get('*', (req, res) => {
+        res.sendFile(path.join(__dirname, 'frontend', 'build', 'index.html'))
+    })
+} else {
+    app.get('/', (req, res) => {
+        res.send('Api running');
+    })
+}
 
 const PORT = process.env.PORT || 5000;
 
