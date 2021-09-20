9f24fd11fbe0db0443567d9b11027250962e733a
Toheeb <toheebyekeen@gmail.com>
2021-09-20 14:20:52 +0100

    added signin route and jwt

--- backend/controller/userController.js
+++ backend/controller/userController.js
@@ -1,12 +1,34 @@
 const userData = require("../data/user");
 const User = require("../models/User");
 const expressAsyncHandler = require("express-async-handler");
+const bcrypt = require("bcryptjs");
+const { generateToken } = require("../utils/token");
 
-const getUsers = expressAsyncHandler(async (req, res) => {
+exports.getUsers = expressAsyncHandler(async (req, res) => {
   // await User.deleteMany({});
 
   const createdUsers = await User.insertMany(userData);
   res.send({ createdUsers });
 });
 
-module.exports = getUsers;
+exports.signin = async (req, res) => {
+  const user = await User.findOne({ email: req.body.email });
+  if (user) {
+    if (bcrypt.compareSync(req.body.password, user.password)) {
+      res.send({
+        _id: user._id,
+        name: user.name,
+        email: user.email,
+        isAdmin: user.isAdmin,
+        token: generateToken(user),
+      });
+      return;
+    }
+  }
+  res.status(401).send({ message: "Invalid email or password" });
+};
+
+// module.exports = {
+//   getUsers,
+//   signin,
+// };
--- backend/routes/userRoutes.js
+++ backend/routes/userRoutes.js
@@ -1,8 +1,9 @@
 const express = require("express");
-const getUsers = require("../controller/userController");
+const { getUsers, signin } = require("../controller/userController");
 
 const router = express.Router();
 
 router.get("/seed", getUsers);
+router.post("/signin", signin);
 
 module.exports = router;
