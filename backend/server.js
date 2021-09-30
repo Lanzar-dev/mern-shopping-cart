ed1c685ca18a601d880d4176362cd72417bea3ef
Toheeb <toheebyekeen@gmail.com>
2021-09-30 23:11:32 +0100

    payment fixed for production

--- backend/server.js
+++ backend/server.js
@@ -24,17 +24,17 @@ app.use((err, req, res, next) => {
   res.status(500).send({ message: err.message });
 });
 
-// if (process.env.NODE_ENV === "production") {
-//   app.use(express.static(path.join(__dirname, "../frontend/build")));
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static(path.join(__dirname, "../frontend/build")));
 
-//   app.get("*", (req, res) => {
-//     res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
-//   });
-// } else {
-//   app.get("/", (req, res) => {
-//     res.send("Api running");
-//   });
-// }
+  app.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, "../frontend", "build", "index.html"));
+  });
+} else {
+  app.get("/", (req, res) => {
+    res.send("Api running");
+  });
+}
 
 const PORT = process.env.PORT || 5000;
 
